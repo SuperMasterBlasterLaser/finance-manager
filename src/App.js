6f90d9a3596a6c9f98e425a40600c20d58a72e12
Alimzhan <alimzhan.amanov@gmail.com>
2017-10-13 13:28:08 +0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Button, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
+		<Col mdOffset={3} xs={12} md={8}>
         <h1>Hello</h1>
+        <Button bsStyle="primary">Primary</Button>
+        </Col>
       </div>
     );
   }
